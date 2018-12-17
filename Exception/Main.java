4419162f2cd52bdead939f2f3c1d381749ffd08d
george <usasne@163.com>
2018-12-17 21:38:37 +0800

    增加异常的Demo

---
+++ Exception/Main.java
@@ -0,0 +1,9 @@
+import demo.*;
+
+public class Main {
+    public static void main(String[]args){
+        String grades[]={new String("70"),new String("101"),new String("85")};
+        ExceptionDemo.main(grades);
+
+    }
+}
