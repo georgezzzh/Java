62ba64201f08936bad92d98859905390fa9d4faf
george <usasne@163.com>
2019-01-31 23:00:07 +0800

    Update HashMapTest.java

--- 集合/HashMapTest.java
+++ 集合/HashMapTest.java
@@ -9,3 +9,12 @@ public class HashMapTest {
         System.out.println(scores.get("Bert"));
     }
 }
+###遍历方法
+  HashMap<String,String> files=new HashMap<String,String>();
+        files.put("key1","v1");
+		files.put("key2","v2");
+		files.put("key3","v3");
+        //HashMap遍历的方法
+        for(Map.Entry<String,String>entry:files.entrySet()){
+           System.out.println(entry.getKey()+entry.getValue());
+        }
