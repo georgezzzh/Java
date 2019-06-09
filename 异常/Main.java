c9c090d060fc42810f970642ecb0494c59ba38eb
george <usasne@163.com>
2019-06-09 18:42:42 +0800

    异常

--- 异常/Main.java
+++
@@ -1,9 +0,0 @@
-import demo.*;
-
-public class Main {
-    public static void main(String[]args){
-        String grades[]={new String("70"),new String("101"),new String("85")};
-        ExceptionDemo.main(grades);
-
-    }
-}
