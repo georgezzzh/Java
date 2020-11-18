317898ab6f1a81cae10e1255f670b0ee0e815555
georgezzzh <usasne@163.com>
2020-11-18 10:10:06 +0800

    rm JUnit

--- JUnit测试/JUnit_Demo/src/test/java/TriangleTest.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
-public class TriangleTest {
-    @Test
-    public void test(){
-        assertEquals(true,new RegularTriangle().isLegal(1,1,3));
-    }
-}
